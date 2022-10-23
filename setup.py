be1aaed31a2a9a15aa70b25825fc67e576880424
Anup Kumar Panwar <1anuppanwar@gmail.com>
2022-10-23 17:41:28 +0530

    v 0.0.2

--- setup.py
+++ setup.py
@@ -1,33 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open('README.md') as readme_file:
+    README = readme_file.read()
 
-setup(
+setup_args = dict(
     name='amazon_data',
-    version='0.0.1',
+    version='0.0.2',
     description='A python package to get amazon product and search data in json form. The package does not require any API keys as it works by scraping the amazon page.',
-    py_modules=["amazon_data"],
-    package_dir={'': 'amazon_data'},
-    extras_require={
-        "dev": [
-            "pytest >= 3.7",
-            "check-manifest",
-            "twine"
-        ]
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ],
-    python_requires='>=3',
-    long_description=long_description,
     long_description_content_type="text/markdown",
-    author="Anup Kumar Panwar",
-    author_email="1anuppanwar@gmail.com",
-    url="https://github.com/AnupKumarPanwar/amazon_data"
+    long_description=README,
+    license='MIT',
+    packages=find_packages(),
+    author='Anup Kumar Panwar',
+    author_email='1anuppanwar@gmail.com',
+    keywords=['Amazon', 'Scraper'],
+    url='https://github.com/AnupKumarPanwar/amazon_data'
 )
+
+install_requires = [
+    'selectorlib',
+    'requests'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
